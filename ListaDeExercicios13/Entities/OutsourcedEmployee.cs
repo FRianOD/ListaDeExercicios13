7949a634830eb4211b972222d348ffee7e70cf1e
FRianOD <rianoliveiracontato@gmail.com>
2023-01-28 16:40:24 -0300

    Adicionar arquivos de projeto.

---
+++ ListaDeExercicios13/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace ListaDeExercicios13.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public sealed override double Payment()
+        {
+            return Hours * ValuePerHour + (AdditionalCharge * 1.1);
+        }
+    }
+}
